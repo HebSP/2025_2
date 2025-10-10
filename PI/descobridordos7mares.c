60eda2a24055e74054293e26e5df1bc6d13efdec
HebSP <hsp2@cin.ufpe.br>
2025-10-10 18:03:38 -0300

    teste1

--- PI/descobridordos7mares.c
+++ PI/descobridordos7mares.c
@@ -5,21 +5,22 @@ int main() {
     int n, x = 0;
     float p, t, g, nota, maiorNota = 0;
     scanf("%d", &n);
-    char *nome = "", *maiorNome = "";
+    char nome[100] = "", maiorNome[100] = "";
     for (int i = 0; i < n; i++) {
-        scanf("%s %f %f %f", nome, &p, &t, &g);
+        scanf("%s %f %f %f", nome, &p, &g, &t);
         nota = (3 * p + g + 2 * t)/6;
-        if (nota > maiorNota && nota >= 3.5) {
-            maiorNota = nota;
-            strcpy(maiorNome, nome);
+        if (nota>= 3.5) {
+            if (nota > maiorNota) {
+                maiorNota = nota;
+                strcpy(maiorNome, nome);
+            }
             x++;
         }
     }
-    printf("Temos %d lugares pra visitar!", x);
+    printf("Temos %d lugares pra visitar!\n", x);
     if (maiorNota < 3.5) {
         printf("Puts. Melhor ficar em casa mesmo");
     } else {
-        printf("Mais de sistema");
-        printf("Devo começar por %s", maiorNome);
+        printf("Devo comecar por %s", maiorNome);
     }
 }
